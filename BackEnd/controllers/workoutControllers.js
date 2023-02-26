fbd151cf0cb50fac490ac935f21c6710fefa80ab
HurasAdam <huradam@wp.pl>
2023-02-26 23:28:05 +0100

    add delete update controller

--- BackEnd/controllers/workoutControllers.js
+++ BackEnd/controllers/workoutControllers.js
@@ -1,5 +1,5 @@
 const Workout = require("../models/workoutModel");
-const mongoose= require('mongoose');
+const mongoose = require("mongoose");
 // create workout
 const createWorkout = async (req, res) => {
   const { title, reps, load } = req.body;
@@ -15,37 +15,50 @@ const createWorkout = async (req, res) => {
 //get all workouts
 
 const getAllWorkouts = async (req, res) => {
-
-  const workouts = await Workout.find({})
-res.status(200).json(workouts)
- 
+  const workouts = await Workout.find({});
+  res.status(200).json(workouts);
 };
 
 //get a single workout
 
 const getSingleWorkout = async (req, res) => {
+  const { id } = req.params;
 
-    const {id}=req.params
-
-
-if(!mongoose.Types.ObjectId.isValid(id)){
-    return res.status(404).json('error:requested resource does not exist')
-}
-const workout = await Workout.findOne({id})
-    res.status(200).json(workout)
-    
-}
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json("error:requested resource does not exist");
+  }
+  const workout = await Workout.findOne({ id });
+  res.status(200).json(workout);
+};
 
 //delete workout
-const deleteWorkout= async(req,res)=>{
-
-  const {id}=req.params;
-  const workout = await Workout.deleteOne({id})
-  res.status(200).json('Usunieto poprawnie');
-
-}
-
+const deleteWorkout = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json("error:requested resource does not exist");
+  }
+  const workout = await Workout.deleteOne({ id });
+  res.status(200).json("Usunieto poprawnie");
+};
 
 //update workout
 
-module.exports = { createWorkout, getAllWorkouts, getSingleWorkout,deleteWorkout };
+const updateWorkout = async (req, res) => {
+  const { id } = req.params;
+
+  const { title, reps, load } = req.body;
+
+  const updatedWorkout = await Workout.findOneAndUpdate({ id },{...req.body})
+
+
+
+  res.status(200).json(updatedWorkout);
+};
+
+module.exports = {
+  createWorkout,
+  getAllWorkouts,
+  getSingleWorkout,
+  deleteWorkout,
+  updateWorkout
+};
--- BackEnd/routes/actions.js
+++ BackEnd/routes/actions.js
@@ -1,16 +1,20 @@
 const express = require("express");
-const {createWorkout,getAllWorkouts,getSingleWorkout,deleteWorkout}= require('../controllers/workoutControllers')
+const {
+  createWorkout,
+  getAllWorkouts,
+  getSingleWorkout,
+  deleteWorkout,
+  updateWorkout,
+} = require("../controllers/workoutControllers");
 const router = express.Router();
 
+router.post("/", createWorkout);
 
+router.get("/:id", getSingleWorkout);
 
-router.post('/',createWorkout)
+router.get("/", getAllWorkouts);
 
+router.delete("/:id", deleteWorkout);
 
-router.get('/:id',getSingleWorkout)
-
-router.get('/',getAllWorkouts)
-
-router.delete('/:id',deleteWorkout)
-
+router.put("/:id", updateWorkout);
 module.exports = router;
