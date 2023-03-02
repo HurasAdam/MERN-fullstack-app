9c85a753ca97dd7b0bbc814f9a7e5f807b28c534
HurasAdam <huradam@wp.pl>
2023-03-02 18:15:21 +0100

    add handle JWT for signup user

--- BackEnd/controllers/userController.js
+++ BackEnd/controllers/userController.js
@@ -1,4 +1,10 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (id) => {
+  const token = jwt.sign({ id }, process.env.SECRET, { expiresIn: "1h" });
+  return token;
+};
 
 //login
 
@@ -8,12 +14,14 @@ const loginUser = (req, res) => {
 
 //register
 
-const signUpUser = async(req, res) => {
+const signUpUser = async (req, res) => {
   const { email, password } = req.body;
 
   try {
     const user = await User.signup(email, password);
-    res.status(200).json({ email, user });
+
+const token= createToken(user._id)
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
--- BackEnd/models/userModel.js
+++ BackEnd/models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose= require('mongoose');
 const bcrypt = require('bcrypt');
-
+const validator = require('validator')
 const Schema = mongoose.Schema;
 const userSchema= new Schema({
 
@@ -17,6 +17,16 @@ const userSchema= new Schema({
 
 
 userSchema.statics.signup= async function(email,password){
+
+if(!email||!password){
+    throw Error("All fields must be filled")
+}
+if(!validator.isEmail(email)){
+    throw Error('Email is not valid')
+}
+if(!validator.isStrongPassword(password)){
+    throw Error('Password not stron enough')
+}
 
 const exists = await this.findOne({email})
 if(exists){
