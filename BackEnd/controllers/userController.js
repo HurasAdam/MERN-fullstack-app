05e9156d72b01fc693322497a2eabce8a3074c2b
HurasAdam <huradam@wp.pl>
2023-03-02 17:29:57 +0100

    handle signup user

--- BackEnd/controllers/userController.js
+++ BackEnd/controllers/userController.js
@@ -1,23 +1,25 @@
+const User = require("../models/userModel");
 
 //login
 
-const loginUser=(req,res)=>{
-
-res.json('zalogowano')
-
-
-}
+const loginUser = (req, res) => {
+  res.json("zalogowano");
+};
 
 //register
 
-const signUpUser= (req,res)=>{
+const signUpUser = async(req, res) => {
+  const { email, password } = req.body;
 
-res.json('zarejestrowano')
+  try {
+    const user = await User.signup(email, password);
+    res.status(200).json({ email, user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
-}
-
-module.exports={
-
-
-    loginUser,signUpUser
-}
+module.exports = {
+  loginUser,
+  signUpUser,
+};
--- BackEnd/models/userModel.js
+++ BackEnd/models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose= require('mongoose');
-const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
+const Schema = mongoose.Schema;
 const userSchema= new Schema({
 
     email:{
@@ -14,4 +15,17 @@ const userSchema= new Schema({
     }
 })
 
-module.exports.mongoose.model('User',userSchema)
+
+userSchema.statics.signup= async function(email,password){
+
+const exists = await this.findOne({email})
+if(exists){
+    throw Error('Email already in use ')
+}
+const salt = await bcrypt.genSalt(10);
+const hash= await bcrypt.hash(password,salt);
+const user = await this.create({email,password:hash})
+return user 
+}
+
+module.exports=mongoose.model('User',userSchema)
